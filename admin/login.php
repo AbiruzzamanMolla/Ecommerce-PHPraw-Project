733a57fdc18af9f02dbf598ee64b213f475988e9
asliabir <abiruzzaman.molla@gmail.com>
2018-02-14 00:16:18 +0600

    Basic Structure and admin login page creation

--- admin/login.php
+++ admin/login.php
@@ -1,19 +1,36 @@
+<?php include "../classes/AdminLogin.php"; ?>
+<?php
+$admin = new AdminLogin();
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+	$adminUser = $_POST['adminUser'];
+	$adminPass = $_POST['adminPass'];
+
+	$loginChk = $admin->adminlogin($adminUser, $adminPass);
+}
+ ?>
 <!DOCTYPE html>
 <head>
 <meta charset="utf-8">
-<title>Login</title>
+<title>Admin Login</title>
     <link rel="stylesheet" type="text/css" href="css/stylelogin.css" media="screen" />
 </head>
 <body>
 <div class="container">
 	<section id="content">
-		<form action="" method="post">
+		<form action="login.php" method="post">
 			<h1>Admin Login</h1>
+			<span style="color:red; font-size:18px;">
+				<?php
+				if(isset($loginmsg)){
+					echo $loginmsg;
+				}
+				?>
+			</span>
 			<div>
-				<input type="text" placeholder="Username" required="" name="username"/>
+				<input type="text" placeholder="Username" name="adminUser"/>
 			</div>
 			<div>
-				<input type="password" placeholder="Password" required="" name="password"/>
+				<input type="password" placeholder="Password" name="adminPass"/>
 			</div>
 			<div>
 				<input type="submit" value="Log in" />
