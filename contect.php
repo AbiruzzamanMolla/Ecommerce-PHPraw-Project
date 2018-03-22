c47e843a89394089af3cc4903f733f42d7f50cb4
asliabir <abiruzzaman.molla@gmail.com>
2018-03-22 23:31:52 +0600

    contrpo

--- contect.php
+++ contect.php
@@ -1,4 +1,6 @@
-
+<?php
+session_start();
+?>
 <?php include "includes/header.php"; ?>
 <?php
 $note ="";
@@ -150,3 +152,65 @@ $note = "Message send succesfully";
         </html>
 
 
+        <?php
+
+if(isset($_POST['register'])){
+
+$c_name = $_POST['c_name'];
+
+$c_email = $_POST['c_email'];
+
+$c_pass = $_POST['c_pass'];
+
+$c_country = $_POST['c_country'];
+
+$c_city = $_POST['c_city'];
+
+$c_contact = $_POST['c_contact'];
+
+$c_address = $_POST['c_address'];
+
+$c_image = $_FILES['c_image']['name'];
+
+$c_image_tmp = $_FILES['c_image']['tmp_name'];
+
+$c_ip = getRealIP();
+
+move_uploaded_file($c_image_tmp,"customer/customer_images/$c_image");
+
+$insert_customer = "insert into customers (customer_name,customer_email,customer_pass,customer_country,customer_city,customer_contact, customer_address, customer_image,customer_ip) values ('$c_name','$c_email','$c_pass','$c_country','$c_city','$c_contact','$c_address','$c_image','$c_ip')";
+
+
+$run_customer = mysqli_query($con, $insert_customer);
+
+$sel_cart = "select * from cart where ip_add='$c_ip'";
+
+$run_cart = mysqli_query($con,$sel_cart);
+
+$check_cart = mysqli_num_rows($run_cart);
+
+if($check_cart>0){
+
+$_SESSION['customer_email']=$c_email;
+
+echo "<script>alert('You have been Registered Successfully')</script>";
+
+echo "<script>window.open('checkout.php','_self')</script>";
+
+}else{
+
+$_SESSION['customer_email']=$c_email;
+
+echo "<script>alert('You have been Registered Successfully')</script>";
+
+echo "<script>window.open('index.php','_self')</script>";
+
+
+}
+
+
+
+
+}
+
+?>
